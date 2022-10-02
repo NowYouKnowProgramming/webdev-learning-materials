96da924492ae3b47545e66475c65f2052d7c4ba6
prplblck <kariurek@gmail.com>
2022-10-02 16:29:15 +0200

    Feat: add  component Promo.tsx base

---
+++ src/components/LandingPage/Promo/Promo.tsx
@@ -0,0 +1,28 @@
+/** @jsxImportSource react */
+
+import classes from './Promo.module.css';
+
+export const Promo = () => {
+	return (
+		<div className={classes.root}>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
+				nam placeat repudiandae. Hic, animi dicta, ab temporibus aliquam a
+				laboriosam inventore quisquam voluptatem cumque autem consequatur
+				minima, mollitia modi placeat.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
+				nam placeat repudiandae. Hic, animi dicta, ab temporibus aliquam a
+				laboriosam inventore quisquam voluptatem cumque autem consequatur
+				minima, mollitia modi placeat.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
+				nam placeat repudiandae. Hic, animi dicta, ab temporibus aliquam a
+				laboriosam inventore quisquam voluptatem cumque autem consequatur
+				minima, mollitia modi placeat.
+			</p>
+		</div>
+	);
+};
