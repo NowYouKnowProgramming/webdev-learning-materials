8e9cbbe2f76bc311b0632cbe72845145dfe319fb
prplblck <kariurek@gmail.com>
2022-10-02 23:10:43 +0200

    WIP: scroll parallax

--- src/components/LandingPage/Promo/Promo.tsx
+++ src/components/LandingPage/Promo/Promo.tsx
@@ -1,22 +1,40 @@
 /** @jsxImportSource react */
 
-import { Parallax } from 'react-scroll-parallax';
+import { useEffect, useState } from 'react';
+import { ScrollParallax } from 'react-just-parallax';
 import classes from './Promo.module.css';
+
+const Component = () => <h2 className={classes.paragraph}>hello</h2>;
 
 export const Promo = () => {
 	return (
 		<div className={classes.root}>
+			<div className={classes.container}>
+				<ScrollParallax isAbsolutelyPositioned>
+					<Component />
+				</ScrollParallax>
+			</div>
+			<p>Siema</p>
 			<p>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos hic
-				voluptatem exercitationem quis nulla veritatis sapiente deleniti ex
-				recusandae, aut commodi dolores at? Cupiditate qui quo sapiente? Enim,
-				voluptate iure!
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
+				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
+				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
 			</p>
-			<Parallax speed={100}>
-				<main>
-					<h2>hello</h2>
-				</main>
-			</Parallax>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
+				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
+				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
+				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
+				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
+				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
+				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
+			</p>
 		</div>
 	);
 };
