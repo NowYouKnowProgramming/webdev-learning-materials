26790541962642f12bd0423df56c21c9fac98658
Karol <kariurek@gmail.com>
2022-10-03 13:11:21 +0200

    Feat: add component ParallaxCard and Promo for landing page + working parallax

--- src/components/LandingPage/Promo/ParallaxCard.tsx
+++ src/components/LandingPage/Promo/ParallaxCard.tsx
@@ -1,8 +1,60 @@
-type Props = {
-	children: React.ReactNode;
+/** @jsxImportSource react */
+
+import { ScrollParallax } from 'react-just-parallax';
+import classes from './ParallaxCard.module.css';
+
+type Image = {
+	src: string;
+	alt: string;
 };
 
-const ParallaxCard = ({ children }: Props) => {
-	return <div>{children}</div>;
+type Props = {
+	title: string;
+	children: React.ReactNode;
+	imageSrc: Image;
+	iconSrc: string | undefined;
+	direction?: 'childrenLeft' | 'childrenRight';
+};
+
+const ParallaxCard = ({
+	children,
+	imageSrc,
+	iconSrc,
+	direction = 'childrenLeft',
+	title,
+}: Props) => {
+	const rootElementDirectionCss =
+		direction === 'childrenLeft' ? classes.left : classes.right;
+
+	const Icons =
+		!!iconSrc &&
+		['One', 'Two', 'Three'].map((icon, index) => {
+			const iconKey = `icon${icon}`;
+
+			const iconCss = `${classes.icon} ${classes[iconKey]}`;
+
+			return (
+				<div className={iconCss} key={index + iconKey}>
+					<img src={iconSrc} alt='' aria-hidden='true' />
+				</div>
+			);
+		});
+
+	return (
+		<div className={[classes.root, rootElementDirectionCss].join(' ')}>
+			<div>
+				<h3>{title}</h3>
+				{children}
+			</div>
+			<div className={classes.imgContainer}>
+				<ScrollParallax strength={0.05}>
+					<img src={imageSrc.src} alt={imageSrc.alt} />
+				</ScrollParallax>
+				<ScrollParallax isAbsolutelyPositioned strength={0.1}>
+					{iconSrc && Icons}
+				</ScrollParallax>
+			</div>
+		</div>
+	);
 };
 export default ParallaxCard;
--- src/components/LandingPage/Promo/Promo.tsx
+++ src/components/LandingPage/Promo/Promo.tsx
@@ -1,40 +1,27 @@
 /** @jsxImportSource react */
 
-import { useEffect, useState } from 'react';
-import { ScrollParallax } from 'react-just-parallax';
+import ParallaxCard from './ParallaxCard';
+import { data } from './Promo.data';
 import classes from './Promo.module.css';
 
-const Component = () => <h2 className={classes.paragraph}>hello</h2>;
-
 export const Promo = () => {
-	return (
-		<div className={classes.root}>
-			<div className={classes.container}>
-				<ScrollParallax isAbsolutelyPositioned>
-					<Component />
-				</ScrollParallax>
-			</div>
-			<p>Siema</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
-				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
-				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
-				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
-				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
-				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
-				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi harum
-				quasi quibusdam beatae quis deserunt similique magnam ex. Corporis qui
-				sit sed optio fuga iusto asperiores nobis officiis harum explicabo!
-			</p>
-		</div>
-	);
+	const Cards = data.map((card, index) => {
+		const cardKey = `card${index + card.title}`;
+
+		const direction = index % 2 === 0 ? 'childrenLeft' : 'childrenRight';
+
+		return (
+			<ParallaxCard
+				key={cardKey}
+				title={card.title}
+				imageSrc={card.imageSrc}
+				iconSrc={card?.iconSrc}
+				direction={direction}
+			>
+				<p>{card.description}</p>
+			</ParallaxCard>
+		);
+	});
+
+	return <div className={classes.root}>{Cards}</div>;
 };
