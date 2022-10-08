c1fbb9331296680c6c52635319a0a6b7f2ea4afa
Willaiem <vvwill4u@gmail.com>
2022-10-09 00:01:37 +0200

    Feat: addutil to generate error objects
    to avoid using any and to be reusable across the app

---
+++ src/utils/unknownToError.ts
@@ -0,0 +1,11 @@
+export const unknownToError = (maybeError: unknown) => {
+  if (maybeError instanceof Error) {
+    return maybeError;
+  }
+
+  if (typeof maybeError === "string") {
+    return new Error(maybeError);
+  }
+
+  return new Error(String(maybeError));
+};
