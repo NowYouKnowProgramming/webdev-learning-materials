a017d29fe6310b6c80ef197279f1d0bb6a991bb3
prplblck <kariurek@gmail.com>
2022-10-14 20:32:48 +0200

    Fix: update puzzle bg img url

--- src/components/LandingPage/Promo/ParallaxPuzzle.tsx
+++ src/components/LandingPage/Promo/ParallaxPuzzle.tsx
@@ -4,7 +4,7 @@ import classes from './ParallaxPuzzle.module.css'
 
 import { ScrollParallax } from 'react-just-parallax'
 
-const src = '/images/promo/4puzzle.png'
+const src = '/images/promo/4puzzle.webp'
 
 export const ParallaxPuzzle = () => {
 	return (
