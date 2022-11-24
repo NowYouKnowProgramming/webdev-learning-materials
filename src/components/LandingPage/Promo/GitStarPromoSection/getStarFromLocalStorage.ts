1bb1d4dd9a1cdb3ff4b364240d8597b75acf4c26
Karol <kariurek@gmail.com>
2022-11-24 10:29:47 +0100

    Fix: change variable name typo in getStarFromLocalStorage

--- src/components/LandingPage/Promo/GitStarPromoSection/getStarFromLocalStorage.ts
+++ src/components/LandingPage/Promo/GitStarPromoSection/getStarFromLocalStorage.ts
@@ -2,8 +2,8 @@ export type StarsInStorage = {
 	starCount: number
 	timestamp: number
 }
-const HOUR_IN_MILISECONDS = 3600000
-const MAX_DATA_OLD_MILISECONDS = HOUR_IN_MILISECONDS
+const HOUR_IN_MILLISECONDS = 3600000
+const MAX_DATA_OLD_MILLISECONDS = HOUR_IN_MILLISECONDS / 2
 
 const verifyStarObject = (starData: any): boolean => {
 	const starCountExist = Object.hasOwn(starData, 'starCount')
@@ -19,7 +19,7 @@ const verifyStarObject = (starData: any): boolean => {
 }
 
 const verifyTimestamp = (timestamp: number): boolean =>
-	Date.now() - timestamp < MAX_DATA_OLD_MILISECONDS
+	Date.now() - timestamp < MAX_DATA_OLD_MILLISECONDS
 
 export const getStarFromLocalStorage = (): null | StarsInStorage => {
 	const savedStars = localStorage.getItem('starCount')
