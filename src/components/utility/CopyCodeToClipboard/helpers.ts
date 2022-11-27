475baef9cd7a4bd929093b6def5ea56dd79b6374
prplblck <kariurek@gmail.com>
2022-11-27 22:47:37 +0100

    Refactor: code snippets component

--- src/components/utility/CopyCodeToClipboard/helpers.ts
+++ src/components/utility/CopyCodeToClipboard/helpers.ts
@@ -15,17 +15,19 @@ export const handleCopyButtonClick = async (
 	button: HTMLButtonElement,
 	text: string
 ) => {
+	const buttonTextsAfterClick = ['Copied', 'Error']
+	const buttonCurrentText = button.innerText
+	if (buttonTextsAfterClick.includes(buttonCurrentText)) return
+
 	try {
 		await navigator.clipboard.writeText(text)
 		button.innerText = 'Copied'
 		button.classList.add('success')
-		setTimeout(() => {
-			button.innerText = 'Copy'
-			button.classList.remove('success')
-		}, 1000)
 	} catch (error) {
 		button.innerText = 'Error'
+	} finally {
 		setTimeout(() => {
+			button.classList.remove('success')
 			button.innerText = 'Copy'
 		}, 1000)
 	}
