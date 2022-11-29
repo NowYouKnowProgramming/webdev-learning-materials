413835aeec0ad94d5e0676841807866c20cc082e
Karol <kariurek@gmail.com>
2022-11-29 08:53:11 +0100

    Fix: update HeadingLinksCopyToClipboard
    change code that was rejected during review
    
    change semantics to remove unnecessary wrapping and better copy button accessibility
    
    update styles to provide better visual consistency for copy button

--- src/components/utility/HeadingLinksCopyToClipboard/helpers.ts
+++ src/components/utility/HeadingLinksCopyToClipboard/helpers.ts
@@ -2,21 +2,20 @@ import { copyIcon } from './copyIconSVG'
 
 export const getHeadingsFromDocument = () => {
 	const mainArticleContainerSelector = '.main-section'
-	const headingsSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-	headingsSelectors.forEach(
-		(heading, index) =>
-			(headingsSelectors[index] = mainArticleContainerSelector + ' ' + heading)
+	const headingsSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'].map(
+		(heading) => `${mainArticleContainerSelector} ${heading}`
 	)
+
 	const headings = document.querySelectorAll<HTMLHeadingElement>(
 		headingsSelectors.join(', ')
 	)
 	return headings
 }
 
-const copyButtonClickHandler = (headingId: string) => {
+const copyButtonClickHandler = async (headingId: string) => {
 	try {
 		window.location.hash = headingId
-		navigator.clipboard.writeText(window.location.href)
+		await navigator.clipboard.writeText(window.location.href)
 	} catch (err) {
 		console.error('Failed to copy: ', err)
 	}
@@ -26,19 +25,21 @@ const createCopyButton = (headingId: string) => {
 	const copyButton = document.createElement('button')
 	copyButton.classList.add('heading-copy')
 	copyButton.innerHTML = copyIcon
+	copyButton.setAttribute('aria-hidden', 'true')
 	copyButton.title = 'Copy heading to clipboard'
 	copyButton.addEventListener('click', () => copyButtonClickHandler(headingId))
 	return copyButton
 }
 
-export const createHeadingWrapper = (heading: HTMLHeadingElement) => {
-	const wrapper = document.createElement('div')
+export const createHeadingWithButton = (heading: HTMLHeadingElement) => {
+	const wrapper = document.createElement(heading.tagName)
 	wrapper.classList.add('heading-wrapper')
-
+	wrapper.textContent = heading.textContent
 	const headingId = heading.id
+	wrapper.id = headingId
+
 	if (headingId) {
-		const headingCopy = heading.cloneNode(true)
-		wrapper.append(headingCopy, createCopyButton(headingId))
+		wrapper.append(createCopyButton(headingId))
 	}
 
 	return wrapper
