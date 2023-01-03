d59000d1e0d59ae72113e90bb43d727ddfbc76d9
Karol <kariurek@gmail.com>
2023-01-03 08:49:09 +0100

    Feat: add oled theme

--- src/components/RightSidebar/ThemeToggleButton.tsx
+++ src/components/RightSidebar/ThemeToggleButton.tsx
@@ -1,8 +1,8 @@
 import type { FunctionalComponent } from 'preact'
-import { useState, useEffect } from 'preact/hooks'
+import { useEffect, useState } from 'preact/hooks'
 import './ThemeToggleButton.css'
 
-const themes = ['light', 'dark']
+const themes = ['light', 'dark', 'oled']
 
 const icons = [
 	<svg
@@ -27,6 +27,20 @@ const icons = [
 	>
 		<path d='M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z' />
 	</svg>,
+	<svg
+		width='20'
+		height='20'
+		viewBox='0 0 15 15'
+		fill='currentColor'
+		xmlns='http://www.w3.org/2000/svg'
+	>
+		<path
+			d='M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM7.49988 1.82689C4.36688 1.8269 1.82707 4.36672 1.82707 7.49972C1.82707 10.6327 4.36688 13.1725 7.49988 13.1726V1.82689Z'
+			fill='currentColor'
+			fill-rule='evenodd'
+			clip-rule='evenodd'
+		></path>
+	</svg>,
 ]
 
 const ThemeToggle: FunctionalComponent = () => {
@@ -47,8 +61,13 @@ const ThemeToggle: FunctionalComponent = () => {
 		const root = document.documentElement
 		if (theme === 'light') {
 			root.classList.remove('theme-dark')
-		} else {
+			root.classList.remove('theme-oled')
+		} else if (theme === 'dark') {
 			root.classList.add('theme-dark')
+			root.classList.remove('theme-oled')
+		} else {
+			root.classList.remove('theme-dark')
+			root.classList.add('theme-oled')
 		}
 	}, [theme])
 
