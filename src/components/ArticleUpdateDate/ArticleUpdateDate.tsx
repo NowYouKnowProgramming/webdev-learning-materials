84f2bef6a25979bf005c5269c7cc8dc4a29b3bb7
Karol <kariurek@gmail.com>
2023-08-25 09:07:37 +0200

    Refactor last update of article date fetching and use SWR

--- src/components/ArticleUpdateDate/ArticleUpdateDate.tsx
+++ src/components/ArticleUpdateDate/ArticleUpdateDate.tsx
@@ -1,36 +1,43 @@
 /** @jsxImportSource react */
 
-import { useEffect, useState } from 'react'
+import useSWR, { Fetcher } from 'swr'
 import { timeSince } from '../../utils/timeSince'
 
 type Props = {
 	path: string
 }
 
-const URL = `https://api.github.com/repos/nowyouknowprogramming/webdev-learning-materials`
-
-const getLastUpdateDate = async (path: string) => {
-	const response = await fetch(`${URL}/commits?path=${path}&per_page=1`)
-	return await response.json()
+type Commit = {
+	commit: {
+		author: {
+			date: string
+		}
+	}
 }
 
+const URL = `https://api.github.com/repos/nowyouknowprogramming/webdev-learning-materials`
+
+const fetcher: Fetcher<Commit[], string> = (...args) =>
+	fetch(...args).then((res) => res.json())
+
 const ArticleUpdateDate = ({ path }: Props) => {
-	const [updatedAt, setUpdatedAt] = useState<null | string>(null)
+	const { data, error, isLoading } = useSWR(
+		`${URL}/commits?path=${path}&per_page=1`,
+		fetcher,
+		{
+			revalidateOnFocus: false,
+			refreshInterval: 3600 * 1000,
+		}
+	)
 
-	useEffect(() => {
-		getLastUpdateDate(path)
-			.then((data) => {
-				setUpdatedAt(data[0].commit.committer.date)
-			})
-			.catch(() => setUpdatedAt(null))
-	}, [])
+	const commitDate = data?.at(0)?.commit.author.date
 
-	if (!updatedAt) return null
+	if (error || !commitDate || isLoading) return null
 
 	return (
 		<p>
 			This article was last updated:{` `}
-			{timeSince(new Date(updatedAt)).replace(`,`, ' and')} ago
+			{timeSince(new Date(commitDate)).replace(`,`, ' and')} ago
 		</p>
 	)
 }
