0feeef2a821ce78e7da902219504766e77887060
Karol <kariurek@gmail.com>
2023-08-25 09:12:26 +0200

    Refactor useArticleDate

--- src/components/ArticleUpdateDate/ArticleUpdateDate.tsx
+++ src/components/ArticleUpdateDate/ArticleUpdateDate.tsx
@@ -1,36 +1,14 @@
 /** @jsxImportSource react */
 
-import useSWR, { Fetcher } from 'swr'
 import { timeSince } from '../../utils/timeSince'
+import { useArticleDate } from './useArticleDate'
 
 type Props = {
 	path: string
 }
 
-type Commit = {
-	commit: {
-		author: {
-			date: string
-		}
-	}
-}
-
-const URL = `https://api.github.com/repos/nowyouknowprogramming/webdev-learning-materials`
-
-const fetcher: Fetcher<Commit[], string> = (...args) =>
-	fetch(...args).then((res) => res.json())
-
 const ArticleUpdateDate = ({ path }: Props) => {
-	const { data, error, isLoading } = useSWR(
-		`${URL}/commits?path=${path}&per_page=1`,
-		fetcher,
-		{
-			revalidateOnFocus: false,
-			refreshInterval: 3600 * 1000,
-		}
-	)
-
-	const commitDate = data?.at(0)?.commit.author.date
+	const { commitDate, error, isLoading } = useArticleDate(path)
 
 	if (error || !commitDate || isLoading) return null
 
