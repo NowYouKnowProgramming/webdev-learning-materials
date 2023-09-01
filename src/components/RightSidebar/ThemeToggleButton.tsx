babc52569a3a11d86e3165d0799211e382d4721e
purple black <contact@purpleblack.dev>
2023-09-01 22:57:24 +0200

    Change theme toggle icon

--- src/components/RightSidebar/ThemeToggleButton.tsx
+++ src/components/RightSidebar/ThemeToggleButton.tsx
@@ -113,7 +113,22 @@ const ThemeToggle = () => {
 				onClick={() => setMobilePickerOpen((open) => !open)}
 				className='desktop-ui:hidden text-theme-base motion-safe:transition cursor-pointer flex items-center justify-center rounded-md bg-theme-base/30 ring-1 ring-basetext/10 min-h-[40px] px-3'
 			>
-				<BlendingModeIcon />
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					height='22'
+					viewBox='0 -960 960 960'
+					width='22'
+				>
+					<defs>
+						<linearGradient id='my-cool-gradient'>
+							<stop offset='0%' stop-color='rgb(190, 100, 0)' />
+							<stop offset='100%' stop-color='rgb(0, 100, 190)' />
+						</linearGradient>
+					</defs>
+					<g fill='url(#my-cool-gradient)'>
+						<path d='M440-80q-33 0-56.5-23.5T360-160v-160H240q-33 0-56.5-23.5T160-400v-280q0-66 47-113t113-47h480v440q0 33-23.5 56.5T720-320H600v160q0 33-23.5 56.5T520-80h-80ZM240-560h480v-200h-40v120q0 17-11.5 28.5T640-600q-17 0-28.5-11.5T600-640v-120h-40v40q0 17-11.5 28.5T520-680q-17 0-28.5-11.5T480-720v-40H320q-33 0-56.5 23.5T240-680v120Zm0 160h480v-80H240v80Zm0-80v80-80Z' />
+					</g>
+				</svg>
 			</button>
 			<AnimatePresence>
 				{mobilePickerOpen && (
