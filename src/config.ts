6a73f4494efb69b4b31b8c67bdc416d24b9ab8f4
Willaiem <vvwill4u@gmail.com>
2023-09-02 19:11:59 +0200

    feat(frontend): added new section - frontend development

--- src/config.ts
+++ src/config.ts
@@ -1,28 +1,28 @@
 export const SITE = {
-	title: 'bewebdev.tech',
-	description: 'The best way to learn web development',
-	defaultLanguage: 'en_US',
+  title: 'bewebdev.tech',
+  description: 'The best way to learn web development',
+  defaultLanguage: 'en_US',
 }
 
 export const OPEN_GRAPH = {
-	image: {
-		src: '/public/default-og-image.png',
-		alt:
-			'astro logo on a starry expanse of space,' +
-			' with a purple saturn-like planet floating in the right foreground',
-	},
-	twitter: 'bewebdev.tech',
+  image: {
+    src: '/public/default-og-image.png',
+    alt:
+      'astro logo on a starry expanse of space,' +
+      ' with a purple saturn-like planet floating in the right foreground',
+  },
+  twitter: 'bewebdev.tech',
 }
 
 // This is the type of the frontmatter you put in the docs markdown files.
 export type Frontmatter = {
-	title: string
-	description: string
-	layout: string
-	image?: { src: string; alt: string }
-	dir?: 'ltr' | 'rtl'
-	ogLocale?: string
-	lang?: string
+  title: string
+  description: string
+  layout: string
+  image?: { src: string; alt: string }
+  dir?: 'ltr' | 'rtl'
+  ogLocale?: string
+  lang?: string
 }
 
 export const GITHUB_EDIT_URL = `https://github.com/NowYouKnowProgramming/webdev-learning-materials/tree/main`
@@ -31,62 +31,63 @@ export const COMMUNITY_INVITE_URL = `https://github.com/NowYouKnowProgramming/we
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
-	indexName: import.meta.env.PUBLIC_INDEX_NAME ?? '',
-	appId: import.meta.env.PUBLIC_APP_ID ?? '',
-	apiKey: import.meta.env.PUBLIC_API_KEY ?? '',
+  indexName: import.meta.env.PUBLIC_INDEX_NAME ?? '',
+  appId: import.meta.env.PUBLIC_APP_ID ?? '',
+  apiKey: import.meta.env.PUBLIC_API_KEY ?? '',
 }
 
 export type Sidebar = Record<string, { text: string; link: string }[]>
 
 export const SIDEBAR: Sidebar = {
-	Start: [{ text: 'Introduction', link: 'introduction' }],
-	Technologies: [
-		{ text: 'HTML', link: 'technologies/html' },
-		{ text: 'CSS', link: 'technologies/css' },
-		{ text: 'JavaScript', link: 'technologies/javascript' },
-		{ text: 'TypeScript', link: 'technologies/typescript' },
-		{ text: 'React', link: 'technologies/react' },
-		{ text: 'Git', link: 'technologies/git' },
-		{ text: 'Node.js', link: 'technologies/nodejs' },
-		{ text: 'Next.js', link: 'technologies/nextjs' },
-		{ text: 'Redux', link: 'technologies/redux' },
-	],
-	Common: [
-		{ text: 'English', link: 'common/english' },
-		{
-			text: 'HR and job seeking tips',
-			link: 'common/hr_recruitment_resources',
-		},
-		{ text: 'Job boards', link: 'common/job_boards' },
-		{ text: 'Learning to learn', link: 'common/learning_to_learn' },
-		{ text: 'Programmer POV', link: 'common/programmer_pov' },
-		{ text: 'Productivity', link: 'common/productivity' },
-	],
-	Principles: [
-		{ text: 'Design patterns', link: 'principles/design_patterns' },
-		{ text: 'Clean code', link: 'principles/clean_code' },
-		{ text: 'Testing', link: 'principles/testing' },
-		{ text: 'UI and UX', link: 'principles/design_ui_ux' },
-		{ text: 'How web works', link: 'principles/how_web_works' },
-	],
-	Software: [
-		{ text: 'Visual Studio Code', link: 'software/vscode' },
-		{ text: 'Vim', link: 'software/vim' },
-		{ text: 'Other tools', link: 'software/other_tools' },
-		{ text: 'Development setup', link: 'software/development_setup' },
-		{ text: 'Repository tools', link: 'software/repository_tools' },
-	],
-	Resources: [
-		{
-			text: 'Project ideas and planning',
-			link: 'resources/build_your_project',
-		},
-		{ text: 'Build a complete project', link: 'resources/complete_project' },
-		{ text: 'Test your skills', link: 'resources/test_your_skills' },
-		{
-			text: 'Content creators',
-			link: 'resources/content_creators',
-		},
-		{ text: 'Other resources', link: 'resources/other_resources' },
-	],
+  Start: [{ text: 'Introduction', link: 'introduction' }],
+  Technologies: [
+    { text: 'HTML', link: 'technologies/html' },
+    { text: 'CSS', link: 'technologies/css' },
+    { text: 'JavaScript', link: 'technologies/javascript' },
+    { text: 'TypeScript', link: 'technologies/typescript' },
+    { text: 'React', link: 'technologies/react' },
+    { text: 'Git', link: 'technologies/git' },
+    { text: 'Node.js', link: 'technologies/nodejs' },
+    { text: 'Next.js', link: 'technologies/nextjs' },
+    { text: 'Redux', link: 'technologies/redux' },
+  ],
+  Common: [
+    { text: 'English', link: 'common/english' },
+    {
+      text: 'HR and job seeking tips',
+      link: 'common/hr_recruitment_resources',
+    },
+    { text: 'Job boards', link: 'common/job_boards' },
+    { text: 'Learning to learn', link: 'common/learning_to_learn' },
+    { text: 'Programmer POV', link: 'common/programmer_pov' },
+    { text: 'Productivity', link: 'common/productivity' },
+  ],
+  Principles: [
+    { text: 'Design patterns', link: 'principles/design_patterns' },
+    { text: 'Clean code', link: 'principles/clean_code' },
+    { text: 'Testing', link: 'principles/testing' },
+    { text: 'UI and UX', link: 'principles/design_ui_ux' },
+    { text: 'How web works', link: 'principles/how_web_works' },
+    { text: 'Frontend development', link: 'principles/frontend' },
+  ],
+  Software: [
+    { text: 'Visual Studio Code', link: 'software/vscode' },
+    { text: 'Vim', link: 'software/vim' },
+    { text: 'Other tools', link: 'software/other_tools' },
+    { text: 'Development setup', link: 'software/development_setup' },
+    { text: 'Repository tools', link: 'software/repository_tools' },
+  ],
+  Resources: [
+    {
+      text: 'Project ideas and planning',
+      link: 'resources/build_your_project',
+    },
+    { text: 'Build a complete project', link: 'resources/complete_project' },
+    { text: 'Test your skills', link: 'resources/test_your_skills' },
+    {
+      text: 'Content creators',
+      link: 'resources/content_creators',
+    },
+    { text: 'Other resources', link: 'resources/other_resources' },
+  ],
 }
---
+++ src/pages/principles/frontend.md
@@ -0,0 +1,43 @@
+---
+title: Frontend development
+description: Everything related to frontend (framework-agnostic) - architecture, patterns, tips and tricks, etc.
+layout: ../../layouts/MainLayout.astro
+---
+
+## Learn about the web
+
+- [web.dev - various resources about front-end from Google itself (responsive design, accessibility, optimization, etc.)](https://web.dev/)
+- [Framework.dev - resources about JavaScript frameworks (books, videos, courses, podcasts), popular libraries and possibility of compare them and more](https://www.framework.dev/)
+
+## Rendering patterns
+
+- [Beyond Fireship - 10 Rendering Patterns for Web Apps](https://www.youtube.com/watch?v=Dkx5ydvtpCA)
+
+## Web Performance
+
+- [Beyond Fireship - The ultimate guide to web performance](https://www.youtube.com/watch?v=0fONene3OIA)
+- [Beyond Fireship - Dramatically improve website speed with Partytown](https://www.youtube.com/watch?v=ZZIR1NGwy-s)
+- [Beyond Fireship -  How to make your JavaScript Bundle Smaller](https://www.youtube.com/watch?v=kwUfeWe7DCw)
+- [⚡️ 💾  Web Performance Snippets](https://github.com/nucliweb/webperf-snippets)
+
+## Architecture
+
+- [Feature-sliced Design](https://feature-sliced.design/)
+- [Dev Agrawal - Jamstack Is Dead... Or Is It? Architecture In Depth (and differences between SSG vs SSR vs SPA)](https://www.youtube.com/watch?v=qAJhkDFODuo)
+
+## Web Performance in Polish
+
+- [Jak zacząć programować? - Co musisz wiedzieć o wydajności stron i aplikacji?](https://www.youtube.com/watch?v=EdPVyF1-7Oc)
+
+## Architecture in Polish
+
+- [Aleksander Patschek - Czysta architektura bez bólu głowy na frontendzie.](https://www.youtube.com/watch?v=Hq3RMXW9Hrc)
+- [Frontend Architecture - ARCHITEKTONICZNE ABC: STRUKTURA KATALOGÓW W PROJEKCIE](https://www.youtube.com/watch?v=GKBvTKG8irU)
+
+## Tools
+
+- [Knip - Find unused files, dependencies and exports in your JavaScript and TypeScript projects.](https://github.com/webpro/knip)
+
+## Other content
+
+- [Josh W. Comeau - The End of Front-End Development](https://www.joshwcomeau.com/blog/the-end-of-frontend-development/)
