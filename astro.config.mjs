40efc46cacbd4589d041c4e9e9e90625068b1dd5
purple black <contact@purpleblack.dev>
2023-09-03 10:05:59 +0200

    Fix prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 //@ts-check
 
-import mdx from '@astrojs/mdx';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
-import compress from 'astro-compress';
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx'
+import preact from '@astrojs/preact'
+import react from '@astrojs/react'
+import compress from 'astro-compress'
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({
-    include: ['**/preact/*']
-  }), react({
-      include: ['**/react/*']
-    }), mdx(), compress(), sitemap(), tailwind()],
-  site: `https://bewebdev.tech/`
-});
+	integrations: [
+		preact({
+			include: ['**/preact/*'],
+		}),
+		react({
+			include: ['**/react/*'],
+		}),
+		mdx(),
+		compress(),
+		sitemap(),
+		tailwind(),
+	],
+	site: `https://bewebdev.tech/`,
+})
