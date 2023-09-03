7362bc2f11d4c411792e853dd40c02eff858a1e8
purple black <contact@purpleblack.dev>
2023-09-03 09:52:18 +0200

    Refactor donation button

--- src/components/DonationModal/DonationModal.tsx
+++ src/components/DonationModal/DonationModal.tsx
@@ -2,13 +2,10 @@
 
 import * as Dialog from '@radix-ui/react-dialog'
 
-import { Cross2Icon, HeartFilledIcon } from '@radix-ui/react-icons'
+import { Cross2Icon } from '@radix-ui/react-icons'
+import { AnimatePresence, motion } from 'framer-motion'
 import { usePersistedState } from '../../hooks/usePersistedState'
-import { motion, AnimatePresence } from 'framer-motion'
-
-const GradientGlow = () => (
-	<div className='scale-0 group-hover:scale-100 transition-transform motion-reduce:duration-0 ease-in-out select-none absolute w-full h-full  bg-gradient-to-r to-grad-start from-grad-end top-0 left-0s motion-safe:animate-pulse blur-md duration-700'></div>
-)
+import { DonationButton } from './DonationButton'
 
 const DonationModal = () => {
 	const [donationDismissed, setDonationDismissed] = usePersistedState(
@@ -29,21 +26,7 @@ const DonationModal = () => {
 	return (
 		<Dialog.Root open={!donationDismissed} onOpenChange={handleOpenChange}>
 			<Dialog.Trigger asChild>
-				<button
-					className='group fixed bottom-5 right-5 z-50 transition-transform motion-safe:hover:scale-105 ease-out shadow-lg'
-					onClick={announcementButtonClickHandler}
-				>
-					<GradientGlow />
-					<span className='relative font-bold bg-gradient-to-r from-grad-start to-grad-end rounded-md motion-reduce:hover:bg-white motion-reduce:hover:bg-none text-white motion-reduce:hover:text-black flex gap-2 py-3 px-8 hover:shadow-2xl items-center'>
-						<div className='relative motion-safe:group-hover:translate-x-11 motion-safe:group-hover:scale-[2.3] transition-transform ease-out'>
-							<HeartFilledIcon className='absolute top-1/5 animate-ping motion-reduce:hidden' />
-							<HeartFilledIcon />
-						</div>
-						<div className='motion-safe:group-hover:opacity-0 transition-opacity'>
-							Support us
-						</div>
-					</span>
-				</button>
+				<DonationButton onClick={announcementButtonClickHandler} />
 			</Dialog.Trigger>
 			<Dialog.Portal forceMount>
 				<AnimatePresence>
