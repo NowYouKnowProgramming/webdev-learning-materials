31ad4b1756893981ea31b909103bbf1fe52bddd3
RezixDev <mateuszrysownik2@gmail.com>
2023-09-14 22:56:08 +0200

    refactor: design and modals

--- src/components/Quiz/AnswerButton.tsx
+++ src/components/Quiz/AnswerButton.tsx
@@ -1,19 +1,28 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 interface AnswerButtonProps {
   children: ReactNode;
   onClick: () => void;
+  gradient: string;
 }
 
-export default function AnswerButton({ children, onClick }: AnswerButtonProps) {
+export const AnswerButton = ({
+  children,
+  onClick,
+  gradient,
+}: AnswerButtonProps) => {
   return (
-    <div className="m-5 relative group" onClick={onClick}>
-      <div className="bg-gradient-to-r to-grad-start from-grad-end absolute left-0 top-0 w-full h-full group-hover:opacity-50 motion-safe:transition-opacity opacity-0 scale-y-125 scale-x-110 pointer-events-none rounded-md duration-700 motion-reduce:duration-0 saturate-200 brightness-125 blur"></div>
-      <a className="transition ease-out group relative block bg-gradient-to-r from-grad-start to-grad-end rounded-md font-bold motion-safe:hover:scale-10">
-        <span className="rounded-md py-3 px-8 text-center group-hover:bg-white ease-out block motion-safe:transition text-white group-hover:text-black">
+    <button className='m-5 relative group' onClick={onClick}>
+      <div className='absolute left-0 top-0 w-full h-full group-hover:opacity-50 motion-safe:transition-opacity opacity-0 scale-y-125 scale-x-110 pointer-events-none rounded-md duration-700 motion-reduce:duration-0 saturate-200 brightness-125 blur'></div>
+      <div
+        className={` group relative block  ${gradient}  rounded-md font-bold motion-safe:hover:scale-10`}
+      >
+        <span className='rounded-md py-3 px-8 text-center group-hover:bg-gradient-to-br  ease-out block motion-safe:transition text-white group-hover:text-black'>
           {children}
         </span>
-      </a>
-    </div>
+      </div>
+    </button>
   );
-}
+};
+
+export default AnswerButton;
--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 
-import AnswerButton from './AnswerButton'
-import questions from './questions/webdev.json'
-import ScoreModal from './ScoreModal'; 
+import AnswerButton from './AnswerButton';
+import questions from './questions/webdev.json';
 
 interface Question {
   question: string;
@@ -10,64 +9,85 @@ interface Question {
   answer: string;
 }
 
-
 export default function Quiz() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
-  const [showModal, setShowModal] = useState(false);
+  const [completed, setCompleted] = useState(false);
 
   const handleAnswerOptionClick = (answer: string) => {
     if (answer === questions[currentQuestion].answer) {
-      setScore((prevScore) => prevScore + 1); // Here's the change
+      setScore((prevScore) => prevScore + 1);
     }
-  
+
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
-      setShowModal(true);
+      setCompleted(true);
     }
   };
-  
+
   const resetQuiz = () => {
     setCurrentQuestion(0);
     setScore(0);
-    setShowModal(false);
+    setCompleted(false);
   };
 
-
   return (
-    <div className="container mx-auto p-4">
-    
-      <div className="question-section bg-purple-900 p-4 rounded relative"> 
-      {showModal && <ScoreModal score={score} totalQuestions={questions.length} onClose={() => setShowModal(false)} />}
-        <div className="question-count text-sm gradient-text font-semibold">
-          <span>Question {currentQuestion + 1}</span>/{questions.length}
+    <div className='w-full max-w-7xl px-8 m-auto py-8 container p-4'>
+      {completed ? (
+        <div className='completed-section p-2 rounded-lg question-section bg-gradient-to-r from-grad-start/30 from-grad-start/10 to-grad-end/10 p-4 rounded relative'>
+          <h2 className='p-5 font-[600] text-2xl'>Quiz Completed!</h2>
+          <p className='py-2 pl-5 text-theme-muted'>Your Score: <span className='font-[600]'>{score}</span></p>
+          <p className='py-2 pl-5 text-theme-muted'>Total Questions: {questions.length}</p>
+          <div className='w-fit ml-auto'>
+            <AnswerButton
+              gradient={
+                ' bg-gradient-to-l from-grad-start/90 from-grad-start/10  to-grad-end/60  '
+              }
+              onClick={resetQuiz}
+            >
+              Reset
+            </AnswerButton>
+          </div>
         </div>
-        <div className="question-text text-xl mt-4">
-          {questions[currentQuestion].question}
-        </div>
-        {/* User's score in the top-right corner */}
-        <div className="absolute top-0 right-0 bg-violet-500 text-white p-2 rounded">
-          Score: {score}
-        </div>
-      </div>
-      <div className="flex-col justify-center	lg:flex-row answer-section flex mt-6 ">
-        {questions[currentQuestion].options.map((option, index) => (
-          <AnswerButton
-            key={index}
-            onClick={() => handleAnswerOptionClick(option)}
-          >
-            {option}
-          </AnswerButton>
-        ))}
-      </div>
-      <div className="w-fit ml-auto">
-        <AnswerButton 
-          onClick={resetQuiz}>
-          Reset
-        </AnswerButton>
-      </div>
+      ) : (
+        <>
+          <div className='p-2 rounded-lg question-section bg-gradient-to-r from-grad-start/30 from-grad-start/10 to-grad-end/10 p-4 rounded relative'>
+            <div className='question-count text-sm gradient-text font-semibold'>
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className='question-text text-xl mt-4'>
+              {questions[currentQuestion].question}
+            </div>
+
+            <div className='absolute top-0 right-0 bg-gradient-to-r from-grad-start/80 to-grad-end/90 text-white p-2 rounded'>
+              Score: {score}
+            </div>
+          </div>
+          <div className='flex-col justify-center	lg:flex-row answer-section flex mt-6 '>
+            {questions[currentQuestion].options.map((option, index) => (
+              <AnswerButton
+                key={index}
+                gradient={' bg-gradient-to-r from-grad-start to-grad-end '}
+                onClick={() => handleAnswerOptionClick(option)}
+              >
+                {option}
+              </AnswerButton>
+            ))}
+          </div>
+          <div className='w-fit ml-auto'>
+            <AnswerButton
+              gradient={
+                ' bg-gradient-to-l from-grad-start/90 from-grad-start/10  to-grad-end/60  '
+              }
+              onClick={resetQuiz}
+            >
+              Reset
+            </AnswerButton>
+          </div>
+        </>
+      )}
     </div>
   );
 }
